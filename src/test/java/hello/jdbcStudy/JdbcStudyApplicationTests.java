94907e3a1cf78f819116eacf417c104f3a76ac7b
mincheol.ha <hmc0501@gmail.com>
2025-03-19 11:06:31 +0900

    Initial commit

---
+++ src/test/java/hello/jdbcStudy/JdbcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.jdbcStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
