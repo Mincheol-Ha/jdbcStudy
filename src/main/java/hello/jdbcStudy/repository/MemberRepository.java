d76884131bf1b51d0febff093b35643d5c33f7a4
Mincheol-Ha <hmc0501@gmail.com>
2025-03-26 16:02:27 +0900

    런타임 예외 적용

---
+++ src/main/java/hello/jdbcStudy/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package hello.jdbcStudy.repository;
+
+import hello.jdbcStudy.domain.Member;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Member findById(String memberId);
+    void update(String memberId, int money);
+    void delete(String memberId);
+}
